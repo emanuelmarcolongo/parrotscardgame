5eb426cf1c48600e1185f0e326a5015232e6203c
emanuel <aegisthesword@gmail.com>
2022-08-19 14:01:50 -0300

    jogo funcional - bugável - sem responsividade

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const qntd = parseInt(prompt('qual a quantidade de cartas (par, entre 4 e 14)'));
-
+let contador = 0;
+let timer = 0;
 // ||
 // while (quantidade < 4 && quantidade > 14 && par !== 0) {
 //     console.log(quantidade);
@@ -29,7 +30,7 @@ const cartas = [];
 
 
 // AQUI EU CRIO O ARRAY COM A QUANTIDADE DE CARTAS SELECIONADAS (EM DUPLAS);
-for (let i=0; i < qntd; i++) {
+for (let i = 0; i < qntd; i++) {
     cartas.push(cards[i]);
 }
 
@@ -43,7 +44,7 @@ function iniciarCartas() {
     for (let i = 0; i < qntd; i++) {
         let elemento = document.querySelector('.conteudo');
         elemento.innerHTML = elemento.innerHTML +
-        `<div class="carta ${cartas[i]}" onclick="virarCarta(this)" >
+            `<div class="carta ${cartas[i]} " onclick="virarCarta(this);setTimeout(compararCartas, 1000, '${cartas[i]}')" >
             <img class='costas'   src="assets/imgs/front.png" alt="">
             <img class='frente'  src="assets/imgs/${cartas[i]}.gif" alt="">
         </div>
@@ -53,12 +54,63 @@ function iniciarCartas() {
 
 iniciarCartas();
 
+const idInterval = setInterval(fimJogo, 100);
+
 function virarCarta(elemento) {
-    elemento.firstElementChild.classList.toggle('esconder');
-    elemento.lastElementChild.classList.toggle('mostrar');
+    elemento.firstElementChild.classList.add('esconder');
+    elemento.lastElementChild.classList.add('mostrar');
+    contador++;
 
 }
 
-function embaralhar() { 
-	return Math.random() - 0.5; 
+let array = [];
+function compararCartas(parametro) {
+    array.push(`.${parametro}`);
+    if (array.length === 2) {
+        if (array[0] === array[1]) {
+            const element = document.querySelectorAll(`${array[0]}`);
+            element[0].classList.add('selecionado', 'selecionado1');
+            element[1].classList.add('selecionado', 'selecionado1');
+            element[0].firstElementChild.classList.add('esconder1');
+            element[0].lastElementChild.classList.add('mostrar1');
+            element[1].firstElementChild.classList.add('esconder1');
+            element[1].lastElementChild.classList.add('mostrar1');
+            element[0].firstElementChild.classList.remove('esconder');
+            element[0].lastElementChild.classList.remove('mostrar');
+            element[1].firstElementChild.classList.remove('esconder');
+            element[1].lastElementChild.classList.remove('mostrar');
+            array = [];
+        } else if (array[0] !== array[1]) {
+            const element1 = document.querySelectorAll(`${array[0]}`);
+            const element2 = document.querySelectorAll(`${array[1]}`);
+            element1[0].firstElementChild.classList.remove('esconder');
+            element1[0].lastElementChild.classList.remove('mostrar');
+            element1[1].firstElementChild.classList.remove('esconder');
+            element1[1].lastElementChild.classList.remove('mostrar');
+            element2[0].firstElementChild.classList.remove('esconder');
+            element2[0].lastElementChild.classList.remove('mostrar');
+            element2[1].firstElementChild.classList.remove('esconder');
+            element2[1].lastElementChild.classList.remove('mostrar');
+            array = [];
+
+
+        }
+    }
+    // AO CLICAR PEGA A CLASSE DA CARTA (CARTA 1, CARTA 2 ETC). PUXAR AS CLASSES COM ARRAY E COMPARAR SE ARRAY[0] === ARRAY[1]
+    //  SE FOR FAZER UM TCHUM E DPS RESETAR TUDO JUNTO COM A FUNÇÃO VIRAR CARTA
+
+}
+
+function fimJogo () {
+    timer = timer + 0.1;
+    const cartasCertas = document.querySelectorAll('.mostrar1');
+    if (cartasCertas.length === cartas.length) {
+        alert (`fim de jogo. ${contador} jogadas e ${timer.toFixed(1)} segundos!` );
+
+        clearInterval(idInterval);
+    }
+}
+
+function embaralhar() {
+    return Math.random() - 0.5;
 }
